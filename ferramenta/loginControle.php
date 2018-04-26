f0ccdcf012a8175a02bb34034c2c52a954c4026b
TH <TH@172.26.76.188>
2018-04-26 11:29:54 -0300

    Correção de bug ao cadastrar um novo usuário

--- ferramenta/loginControle.php
+++ ferramenta/loginControle.php
@@ -11,9 +11,15 @@ class Usuarios extends Banco{
     public function registrarUsuario($nome, $email, $profissao, $senha){
         $sql = "INSERT INTO login(codlogin, nome, profissao, login, senha, bloqueado, admin) VALUES (DEFAULT,'". $nome ."','". $profissao ."','". $email ."',MD5('". $senha ."'),DEFAULT,DEFAULT)";
         parent::Executar($sql);
-        $_SESSION['login'] = $email;
-        $_SESSION['loginNome'] = $nome;
-        $_SESSION['isAdmin'] = 0;
+        
+        $sql = "SELECT * FROM login WHERE login = '" . $email . "';";
+        $rtn = parent::Executar($sql);
+        $usuario = mysqli_fetch_row($rtn);
+        
+        $_SESSION['login'] = $usuario[0];
+        $_SESSION['loginNome'] = $usuario[1];
+        $_SESSION['isAdmin'] = $usuario[6];
         header("location:boasvindas.php");
+        
     }
 }
